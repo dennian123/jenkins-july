f5be76f13892b65907958598dda3c7291f622323
dennian123 <145859371+dennian123@users.noreply.github.com>
2023-12-20 10:00:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "web" {
-  ami = "ami-092b51d9008adea15"
+  ami = "ami-0ee4f2271a4df2d7d"
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
   key_name = "my-laptop-key"
